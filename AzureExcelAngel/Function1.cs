0af41e036350f7a44433ef04939dd1ce8001f11b
Angel Pinto <pintodiazangel@gmail.com>
2026-05-04 12:48:06 +0200

    funcion para generar graficos en blob

--- AzureExcelAngel/Function1.cs
+++ AzureExcelAngel/Function1.cs
@@ -17,12 +17,38 @@ public class Function1
     {
         _logger = logger;
     }
+    private byte[] GenerarGraficoPng
+        (Dictionary<string, int> totalesPorCliente)
+    {
+        var plt = new ScottPlot.Plot();
 
+        double[] valores = totalesPorCliente.Values.Select(v => (double)v).ToArray();
+        string[] etiquetas = totalesPorCliente.Keys.ToArray();
+
+        var pie = plt.Add.Pie(valores);
+        pie.SliceLabelDistance = 1.3;
+
+        // Asignar etiquetas a cada slice
+        for (int i = 0; i < pie.Slices.Count; i++)
+        {
+            pie.Slices[i].Label = $"{etiquetas[i]}\n({valores[i]:N0}€)";
+            pie.Slices[i].LegendText = etiquetas[i];
+        }
+
+        plt.Title("Total de Precio por Cliente");
+        plt.ShowLegend();
+        plt.Axes.Frameless();
+        plt.HideAxesAndGrid();
+
+        return plt.GetImageBytes(600, 400, ScottPlot.ImageFormat.Png);
+    }
     [Function(nameof(Function1))]
-    public async Task Run([BlobTrigger("excel-files/{name}", Connection = "StorageAngel")] Stream stream, string name)
+    [BlobOutput("graficos-excel/{name}.png", Connection = "StorageAngel")]
+    public async Task Run([BlobTrigger("excel-files/{name}", Connection = "StorageAngel")] BinaryData blobData, string name, Stream outputBlob)
     {
         ExcelPackage.License.SetNonCommercialPersonal("Angel");
         //CREAMOS PACKAGE PARA LEER EL XLSX 
+        using var stream = blobData.ToStream();
         ExcelPackage package = new ExcelPackage(stream);
         ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
         string connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=sa;Encrypt=True;Trust Server Certificate=True";
@@ -37,29 +63,26 @@ public class Function1
         //EXCEL COMIENZA SUS FILAS EN 1 (NUESTRA CABECERA) 
         for (int i = 2; i <= worksheet.Dimension.Rows; i++)
         {
-
             string cliente = worksheet.Cells[i, 1].Text;
-
             string concepto = worksheet.Cells[i, 2].Text;
-
             int precio = int.Parse(worksheet.Cells[i, 3].Text);
-
             com.Parameters.AddWithValue("@cliente", cliente);
-
             com.Parameters.AddWithValue("@concepto", concepto);
-
             com.Parameters.AddWithValue("@precio", precio);
-
             com.ExecuteNonQuery();
-
             com.Parameters.Clear();
-
             _logger.LogInformation("Cliente: " + cliente);
-
         }
-
-
-
+                //CREAMOS UN NUEVO DICCIONARIO
+        var totalesPorCliente = new Dictionary<string, int>();
+ 
+        // Generar PNG y subir a Blob
+        byte[] png = GenerarGraficoPng(totalesPorCliente);
+        await outputBlob.WriteAsync(png);
+ 
+        _logger.LogInformation($"Gráfico PNG guardado en: graficos-output/{name}.png");
+  
         cn.Close();
     }
+
 }
